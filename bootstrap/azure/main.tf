29c8c0ee8d5346849f135ea1446bf401d77fd1dd
evolvingsewage <carlp941@gmail.com>
2026-07-05 17:04:06 -0600

    first crack at azure

---
+++ bootstrap/azure/main.tf
@@ -0,0 +1,38 @@
+data "azurerm_client_config" "current" {}
+
+locals {
+  subscription_id_short = substr(replace(data.azurerm_client_config.current.subscription_id, "-", ""), 0, 8)
+  storage_account_name  = "${var.storage_account_prefix}${local.subscription_id_short}"
+}
+
+resource "azurerm_resource_group" "state" {
+  name     = var.resource_group_name
+  location = var.azure_region
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "azurerm_storage_account" "state" {
+  name                     = local.storage_account_name
+  resource_group_name      = azurerm_resource_group.state.name
+  location                 = azurerm_resource_group.state.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2"
+
+  blob_properties {
+    versioning_enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_id    = azurerm_storage_account.state.id
+  container_access_type = "private"
+}
---
+++ bootstrap/azure/variables.tf
@@ -0,0 +1,15 @@
+variable "azure_region" {
+  type    = string
+  default = "eastus"
+}
+
+variable "resource_group_name" {
+  type    = string
+  default = "infra-perez-wiki-tfstate-rg"
+}
+
+variable "storage_account_prefix" {
+  description = "Prefix for the storage account name. Azure storage account names are 3-24 chars, lowercase letters/numbers only -- no hyphens, unlike S3 bucket names."
+  type        = string
+  default     = "infraperezwiki"
+}
